2981b5acc0bb045ae70cee90889ef6665856debe
PKU-Heliosphere <74942569+PKU-Heliosphere@users.noreply.github.com>
2020-12-01 20:05:30 +0800

    Add files via upload

---
+++ pkues_velocity.m
@@ -0,0 +1,146 @@
+% 20-05-14 08:23 Codeded By Xingyu Zhu and Jiansen He
+% This file calculates the current/velocity of each component
+
+
+% % -- main program begin to set the matrix elements --
+k=sqrt(kz^2+kx^2);
+bsab=kx*rhocsab;
+bsab(abs(bsab)<1e-50)=1e-50;  % to avoid singular when k_perp=0
+
+bsab2=bsab.^2;
+M=sparse(NN,NN);
+
+% initialize
+b11_s=zeros(S); b12_s=zeros(S); b13_s=zeros(S); 
+b21_s=zeros(S); b22_s=zeros(S); b23_s=zeros(S); 
+b31_s=zeros(S); b32_s=zeros(S); b33_s=zeros(S);
+cnj_s=zeros(3*SNJ,S);
+b11nj_s=cnj_s.*0; b12nj_s=cnj_s.*0; b13nj_s=cnj_s.*0;
+b21nj_s=cnj_s.*0; b22nj_s=cnj_s.*0; b23nj_s=cnj_s.*0;
+b31nj_s=cnj_s.*0; b32nj_s=cnj_s.*0; b33nj_s=cnj_s.*0;
+
+for s=1:S % species
+  nj=0;
+  for n=-N:N % Bessel function
+    %             Gamn=exp(-bs2(s))*besseli(n,bs2(s)); % large k_perp will NaN
+    %             Gamnp=exp(-bs2(s))*(besseli(n+1,bs2(s))+...
+    %                 besseli(n-1,bs2(s))-2*besseli(n,bs2(s)))/2;
+    
+    for j=1:J % poles of Z(zeta)
+      nj=nj+1;
+      
+      for iab=1:2 % 2018-10-13 10:58
+      
+      Gamn=besseli(n,bsab2(iab,s),1); % 2014-10-13 12:51
+      Gamnp=(besseli(n+1,bsab2(iab,s),1)+...
+        besseli(n-1,bsab2(iab,s),1)-2*besseli(n,bsab2(iab,s),1))/2;
+  
+      cnj_s(nj,s)=czj(j)*kz*vtzs(s)+kz*vds(s)+n*wcs(s); %
+
+      cnj=cnj_s(nj,s);
+      bj0ab=vds(s)+(1-1/lmdTab(iab,s))*czj(j)*vtzs(s);
+
+      %
+      if(n==0)  % for A_nj
+        bnj1=bj0ab/(czj(j)*vtzs(s)+vds(s)); % avoid cnj=0
+      else
+        bnj1=kz*bj0ab/cnj;
+      end
+      bnj2=1-bnj1;
+
+      tmp=wps2(s)*bzj(j);
+
+      b11nj_s(nj,s)=b11nj_s(nj,s)+rsab(iab,s)*tmp*bnj2*n^2*Gamn/bsab2(iab,s);
+      b11_s(s)=b11_s(s)+rsab(iab,s)*tmp*bnj1*n^2*Gamn/bsab2(iab,s);
+
+      b12nj_s(nj,s)=b12nj_s(nj,s)+rsab(iab,s)*tmp*bnj2*1i*n*Gamnp;
+      b12_s(s)=b12_s(s)+rsab(iab,s)*tmp*bnj1*1i*n*Gamnp;
+      b21nj_s(nj,s)=-b12nj_s(nj,s);
+      b21_s(s)=-b12_s(s);
+
+      b22nj_s(nj,s)=b22nj_s(nj,s)+rsab(iab,s)*tmp*bnj2*(n^2*Gamn/bsab2(iab,s)...
+          -2*bsab2(iab,s)*Gamnp);
+      b22_s(s)=b22_s(s)+rsab(iab,s)*tmp*bnj1*(n^2*Gamn/bsab2(iab,s)...
+          -2*bsab2(iab,s)*Gamnp);
+
+      %
+      if(n==0)  % for eta_n*A_nj
+        bnj1=0; % avoid cnj=0 when kz=0
+      else
+        bnj1=n*wcs(s)*bj0ab/cnj/vtzs(s);
+      end
+      bnj2=czj(j)/lmdTab(iab,s)+bnj1; %
+
+      b13nj_s(nj,s)=b13nj_s(nj,s)+rsab(iab,s)*tmp*bnj2*n*...
+          sqrt(2*lmdTab(iab,s))*Gamn/bsab(iab,s);
+      b13_s(s)=b13_s(s)-rsab(iab,s)*tmp*bnj1*n*sqrt(2*lmdTab(iab,s))*Gamn/bsab(iab,s); %
+      b31nj_s(nj,s)=b13nj_s(nj,s);
+      b31_s(s)=b13_s(s);
+
+      b23nj_s(nj,s)=b23nj_s(nj,s)-rsab(iab,s)*1i*tmp*bnj2*...
+          sqrt(2*lmdTab(iab,s))*Gamnp*bsab(iab,s);
+      b23_s(s)=b23_s(s)+rsab(iab,s)*1i*tmp*bnj1*sqrt(2*lmdTab(iab,s))*Gamnp*bsab(iab,s); %
+      b32nj_s(nj,s)=-b23nj_s(nj,s);
+      b32_s(s)=-b23_s(s);
+
+      %
+      if(bj0ab==0 || kz==0)  % for eta_n^2*A_nj
+        bnj1=0;
+        bnj2=czj(j)*czj(j);
+      else
+        % bnj1=n^2*bj0ab/cnj/vtzs(s)^2/kz;
+        bnj1=n^2*wcs(s)^2*bj0ab/cnj/vtzs(s)^2/kz; % !!fixed bug of missed wcs^2, 18-10-01 10:19
+        bnj2=(vds(s)/vtzs(s)+czj(j))*czj(j)/lmdTab(iab,s)+...
+          n*wcs(s)*bj0ab*(1-n*wcs(s)/cnj)/vtzs(s)^2/kz; %
+      end
+
+      b33nj_s(nj,s)=b33nj_s(nj,s)+rsab(iab,s)*tmp*bnj2*2*lmdTab(iab,s)*Gamn;
+      b33_s(s)=b33_s(s)+rsab(iab,s)*tmp*bnj1*2*lmdTab(iab,s)*Gamn;
+      end
+    end
+  end
+end
+
+
+wtmp = wws2(jpa,1,jpl)*wcs1;
+dEx_tmp=Pola_SI(jpa,1,jpl,1); dEy_tmp=Pola_SI(jpa,1,jpl,2); dEz_tmp=Pola_SI(jpa,1,jpl,3);
+dBx_tmp=Pola_SI(jpa,1,jpl,4); dBy_tmp=Pola_SI(jpa,1,jpl,5); dBz_tmp=Pola_SI(jpa,1,jpl,6);
+for s=1:S
+    Js(jpa,1,s,jpl)=0.0; Js(jpa,2,s,jpl)=0.0; Js(jpa,3,s,jpl)=0.0;
+    Jxs=0.0; Jys=0.0; Jzs=0.0;
+    nj=0;
+    for n=-N:N
+        for j=1:J
+            nj=nj+1;
+            Jxs=Jxs+b11nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEx_tmp+...
+                b12nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEy_tmp+...
+                b13nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEz_tmp;
+            Jys=Jys+b21nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEx_tmp+...
+                b22nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEy_tmp+...
+                b23nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEz_tmp;
+            Jzs=Jzs+b31nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEx_tmp+...
+                b32nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEy_tmp+...
+                b33nj_s(nj,s)/(wtmp-cnj_s(nj,s))*dEz_tmp;
+        end
+    end
+    Js(jpa,1,s,jpl)=-1i*epsilon0*(Jxs+b11_s(s)/wtmp*dEx_tmp+b12_s(s)/wtmp*dEy_tmp+b13_s(s)/wtmp*dEz_tmp);
+    Js(jpa,2,s,jpl)=-1i*epsilon0*(Jys+b21_s(s)/wtmp*dEx_tmp+b22_s(s)/wtmp*dEy_tmp+b23_s(s)/wtmp*dEz_tmp);
+    Js(jpa,3,s,jpl)=-1i*epsilon0*(Jzs+b31_s(s)/wtmp*dEx_tmp+b32_s(s)/wtmp*dEy_tmp+b33_s(s)/wtmp*dEz_tmp);
+    dV(jpa,1,s,jpl)=Js(jpa,1,s,jpl)/(qs(s)*ns0(s));
+    dV(jpa,2,s,jpl)=Js(jpa,2,s,jpl)/(qs(s)*ns0(s));
+    dV(jpa,3,s,jpl)=Js(jpa,3,s,jpl)/(qs(s)*ns0(s));
+    dVnorm(jpa,1,s,jpl)=dV(jpa,1,s,jpl)/vA;
+    dVnorm(jpa,2,s,jpl)=dV(jpa,2,s,jpl)/vA;
+    dVnorm(jpa,3,s,jpl)=dV(jpa,3,s,jpl)/vA;
+    %density fluctuation
+    xinorm(jpa,s,jpl) = (dV(jpa,1,s,jpl)*kx+dV(jpa,3,s,jpl)*kz)/wtmp;
+    % dissipation
+    JE(jpa,1,s,jpl)=-(Js(jpa,1,s,jpl)*conj(dEx_tmp)+conj(Js(jpa,1,s,jpl))*dEx_tmp)/4;
+    JE(jpa,2,s,jpl)=-(Js(jpa,2,s,jpl)*conj(dEy_tmp)+conj(Js(jpa,2,s,jpl))*dEy_tmp)/4;
+    JE(jpa,3,s,jpl)=-(Js(jpa,3,s,jpl)*conj(dEz_tmp)+conj(Js(jpa,3,s,jpl))*dEz_tmp)/4;
+    EBenergy=(dEx_tmp*conj(dEx_tmp)+dEy_tmp*conj(dEy_tmp)+dEz_tmp*conj(dEz_tmp))*epsilon0*0.5+...
+        (dBx_tmp*conj(dBx_tmp)+dBy_tmp*conj(dBy_tmp)+dBz_tmp*conj(dBz_tmp))/mu0*0.5;
+    JE(jpa,1,s,jpl)=JE(jpa,1,s,jpl)/EBenergy/2;
+    JE(jpa,2,s,jpl)=JE(jpa,2,s,jpl)/EBenergy/2;
+    JE(jpa,3,s,jpl)=JE(jpa,3,s,jpl)/EBenergy/2;
+end
---
+++ pkues_write_f_SI_unit.m
@@ -0,0 +1,187 @@
+% written by Xingyu Zhu and Jiansen He 2020-06-01
+% last modified: 2020-06-19
+%%%%%%%%%%%%%%%%%%%%%%%
+VXarray = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+VYarray = zeros(vxsteps+1,vysteps+1,vzsteps+1);                                                                                                                                                                                                                     rray = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+VZarray = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+f0array = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+farray  = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+imfarray= zeros(vxsteps+1,vysteps+1,vzsteps+1);
+dfarray = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+% dfqEarray = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+% dfqvBarray = zeros(vxsteps+1,vysteps+1,vzsteps+1);
+
+%%%%%%%%%%%%%%%%%%%%%%%
+x = wws(jpa_df,jpb_df,jpl_df)*wcs1;
+dEx = Pola_SI(jpa_df,jpb_df,jpl_df,1)/scaling_factor;
+dEy = Pola_SI(jpa_df,jpb_df,jpl_df,2)/scaling_factor;
+dEz = Pola_SI(jpa_df,jpb_df,jpl_df,3)/scaling_factor;
+dBx = Pola_SI(jpa_df,jpb_df,jpl_df,4)/scaling_factor;
+dBy = Pola_SI(jpa_df,jpb_df,jpl_df,5)/scaling_factor;
+dBz = Pola_SI(jpa_df,jpb_df,jpl_df,6)/scaling_factor;
+
+% Start Time series:
+maxf=-999.;
+minf=999.;
+maxdfim=-999.;
+mindfim=999.;
+
+j=s_df;
+
+for timerun=0:num_periods*timesteps
+
+    if (periods)
+        time=2.d0*pi*(1.d0*timerun)/(abs(real(x))*1.d0*timesteps);
+    else
+        time=2.d0*pi*(1.d0*timerun)/(real(1.d0)*1.d0*timesteps);
+    end
+	disp(['time check',num2str(time)])
+
+    filename=['pdrk_dist_deltaf_(kdi=' num2str(k*cwp,'%.3f') ...
+        ')(ampl=' num2str(ampl,'%.2f') ')_' num2str(timerun,'%03d') '.vtk'];
+    filename2=['pdrk_normEB_(kdi=' num2str(k*cwp,'%.3f') ...
+        ')(ampl=' num2str(ampl,'%.2f') ')_' num2str(timerun,'%03d') '.vtk'];
+    disp(['kdi=' num2str(k*cwp,'%.3f') ';' ...
+        'wr/wci=' num2str(real(x/wcs1),'%.3f') ';' ...
+        'wi/wci=' num2str(imag(x/wcs1),'%.3f')]);
+
+    % Here should the loop over all v start:
+    for ii=0:vxsteps
+    for kk=0:vysteps
+    for ll=0:vzsteps
+
+    vx=vxrange(1)+(1.d0*ii)*(vxrange(2)-vxrange(1))/(1.d0*vxsteps); % ¡¾vA¡¿
+    vy=vyrange(1)+(1.d0*kk)*(vyrange(2)-vyrange(1))/(1.d0*vysteps); % ¡¾vA¡¿
+    vz=vzrange(1)+(1.d0*ll)*(vzrange(2)-vzrange(1))/(1.d0*vzsteps); % ¡¾vA¡¿
+    vx=vx*vA; vy=vy*vA; vz=vz*vA; % ¡¾m¡¿
+
+    vpar=vz; % ¡¾m¡¿
+    vperp=sqrt(vx*vx+vy*vy); % ¡¾m¡¿
+    phi=acos(vx/vperp);
+    if vy<0.d0
+        phi=2.d0*pi-phi; end
+    if vperp==0.d0
+        phi=0.d0; end
+
+    dfdvpar=-2.d0*(vpar-vds(j))*exp(-vperp^2/vthpS(j)^2);
+    dfdvpar=dfdvpar*exp(-(vpar-vds(j))^2/vthzS(j)^2);
+    dfdvpar=dfdvpar/(pi^1.5*vthpS(j)^2*vthzS(j)^3)*ns0(j);
+                     
+    dfdvperp=-2.d0*vperp*exp(-vperp^2/vthpS(j)^2);
+    dfdvperp=dfdvperp*exp(-(vpar-vds(j))^2/vthzS(j)^2);
+    dfdvperp=dfdvperp/(pi^1.5*vthpS(j)^4*vthzS(j))*ns0(j);
+
+    fnull=1.d0/(vthpS(j)^2*vthzS(j)*pi^1.5);
+    fnull=fnull*exp(-(vpar-vds(j))^2/vthzS(j)^2)*exp(-vperp^2/vthpS(j)^2)*ns0(j);
+
+    z=kx*vperp/wcs(j);
+
+    %Summation over all Bessel functions:
+    deltaf=0.d0;% dfqE=0.d0;dfqvB=0.d0;
+    for m=-N:N
+
+        a=(1.d0*m)*wcs(j)-x+kz*vpar;
+
+        UStix=dfdvperp+kz*(vperp*dfdvpar-vpar*dfdvperp)/x;
+        VStix=kx*(vperp*dfdvpar-vpar*dfdvperp)/x;
+%         qEStix = dfdvperp;
+%         qvBStix1 = kz*(vperp*dfdvpar-vpar*dfdvperp)/x;
+
+        comp1=dEx*UStix*(a*1i*cos(phi)-wcs(j)*sin(phi))/(wcs(j)^2-a^2);
+        comp2=dEy*UStix*(a*1i*sin(phi)+wcs(j)*cos(phi))/(wcs(j)^2-a^2);
+        comp3=-1i*dEz*dfdvpar/a;
+        comp4=-dEz*VStix*(a*1i*cos(phi)-wcs(j)*sin(phi))/(wcs(j)^2-a^2);
+        
+%         comp11=dEx*qEStix*(a*1i*cos(phi)-wcs(j)*sin(phi))/(wcs(j)^2-a^2);
+%         comp12=dEy*qEStix*(a*1i*sin(phi)+wcs(j)*cos(phi))/(wcs(j)^2-a^2);
+%         comp13=comp3;
+%         
+%         comp21=dEx*qvBStix1*(a*1i*cos(phi)-wcs(j)*sin(phi))/(wcs(j)^2-a^2);
+%         comp22=dEy*qvBStix1*(a*1i*sin(phi)+wcs(j)*cos(phi))/(wcs(j)^2-a^2);
+%         comp23=comp4;
+        
+
+        deltaf = deltaf - qs(j)/ms(j)*exp(1i*z*sin(phi))*exp(-1i*(1.d0*m)*phi)*besselj(m,z)*ampl*(comp1+comp2+comp3+comp4);
+%         dfqE = dfqE - qs(j)/ms(j)*exp(1i*z*sin(phi))*exp(-1i*(1.d0*m)*phi)*besselj(m,z)*ampl*(comp11+comp12+comp13);
+%         dfqvB = dfqvB - qs(j)/ms(j)*exp(1i*z*sin(phi))*exp(-1i*(1.d0*m)*phi)*besselj(m,z)*ampl*(comp21+comp22+comp23);
+    end % end m loop
+
+    if const_r
+        if damping
+            deltaf=deltaf*exp(-1i*time*x);
+%             dfqE=dfqE*exp(-1i*time*x);
+%             dfqvB=dfqvB*exp(-1i*time*x);
+        else
+            deltaf=deltaf*exp(-1i*time*real(x));
+%             dfqE=dfqE*exp(-1i*time*real(x));
+%             dfqvB=dfqvB*exp(-1i*time*real(x));
+        end
+    else 
+        deltaf=deltaf*exp(-1i*2.d0*pi*(1.d0*timerun)/(1.d0*timesteps));
+%         dfqE=dfqE*exp(-1i*2.d0*pi*(1.d0*timerun)/(1.d0*timesteps));
+%         dfqvB=dfqvB*exp(-1i*2.d0*pi*(1.d0*timerun)/(1.d0*timesteps));
+    end
+
+    maxf=max((maxf),real(fnull+deltaf));
+    minf=min((minf),real(fnull+deltaf));
+    maxdfim=max((maxdfim),imag(deltaf));
+    mindfim=min((mindfim),imag(deltaf));
+
+	VXarray(ii+1,kk+1,ll+1)=real(vx);
+	VYarray(ii+1,kk+1,ll+1)=real(vy);
+	VZarray(ii+1,kk+1,ll+1)=real(vz);
+    f0array(ii+1,kk+1,ll+1)=real(fnull);
+	farray(ii+1,kk+1,ll+1)=real(fnull+deltaf);
+    imfarray(ii+1,kk+1,ll+1)=imag(fnull+deltaf);
+    dfarray(ii+1,kk+1,ll+1)=real(deltaf);
+%     dfqEarray(ii+1,kk+1,ll+1)=real(dfqE);
+%     dfqvBarray(ii+1,kk+1,ll+1)=real(dfqvB);
+    end % end vz loop
+    end % end vy loop
+    end % end vx loop
+    
+    disp(['# Maximum value of f:',num2str(maxf)]);
+    disp(['# Minimum value of f:',num2str(minf)]);
+    disp(['# Maximum value of Im(delta f):',num2str(maxdfim)]);
+    disp(['# Minimum value of Im(delta f):',num2str(mindfim)]);
+
+    title1 = 'f0+deltaf';
+    title2 = 'deltaf';
+%     title3 = 'dfqE';
+%     title4 = 'dfqvB';
+
+%% save to vtk file to be visulized by Paraview
+    E_direc = vxrange(2)*real([dEx,dEy,dEz]*exp(-1i*time*real(x))/norm(real([dEx,dEy,dEz])));
+    B_direc = vxrange(2)*real([dBx,dBy,dBz]*exp(-1i*time*real(x))/norm(real([dBx,dBy,dBz])));
+    filename = [savepath filename];
+    filename2 = [savepath filename2];
+    vtkwrite(filename,'structured_grid',VXarray/vA,VYarray/vA,VZarray/vA,...
+        'scalars',title1,farray,'scalars',title2,dfarray,'Precision',20,'BINARY');
+    %%% write E field and B field
+    fid = fopen(filename2, 'w'); 
+    % VTK files contain five major parts
+    % 1. VTK DataFile Version
+    fprintf(fid, '# vtk DataFile Version 2.0\n');
+    % 2. Title
+    fprintf(fid, 'VTK from Matlab\n');
+    fprintf(fid, 'ASCII\n');
+    fprintf(fid, 'DATASET POLYDATA\n');
+    fprintf(fid, ['POINTS ' num2str(3) ' float\n']);
+    spec = [repmat(['%0.', '15', 'f '], 1, 3), '\n'];   
+    fprintf(fid, spec, [0.0,0.0,0.0]);
+    fprintf(fid, spec, E_direc);
+    fprintf(fid, spec, B_direc);
+    fprintf(fid,'\nLINES %d %d\n',2,6);
+    fprintf(fid,'2 %d %d\n',[0;1]);
+    fprintf(fid,'2 %d %d\n',[0;2]);
+    fprintf(fid,'\nCELL_DATA 2\n');
+    fprintf(fid,'SCALARS cell_scalars float 1\n');
+    fprintf(fid,'LOOKUP_TABLE my_table\n');
+    fprintf(fid,'0.0\n');
+    fprintf(fid,'1.0\n');
+    fprintf(fid,'\nLOOKUP_TABLE my_table 2\n');
+    fprintf(fid,'0.0 1.0 0.0 1.0\n');
+    fprintf(fid,'1.0 1.0 0.0 1.0');
+    fclose(fid);
+
+end
